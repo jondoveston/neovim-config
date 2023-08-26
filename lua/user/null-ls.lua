c5cab12e0fb4102b2ce052262b95b3473310b0c6
Jon Doveston <jon@doveston.me.uk>
2023-08-26 23:21:30 +0100

    Add lsp servers and null-ls tools
    This is a WIP. I have added many (not all) possible servers and tools for
    various filetypes I work on. I suspect many will conflict so they are commented
    out until I have time to assess them.
    
    I also added the mason null-ls plugin to auto install configured null-ls tools.

--- lua/user/null-ls.lua
+++ lua/user/null-ls.lua
@@ -5,29 +5,148 @@ local M = {
     {
       "nvim-lua/plenary.nvim",
     },
+    {
+      "jay-babu/mason-null-ls.nvim",
+      event = { "BufReadPre", "BufNewFile" },
+    },
   },
 }
 
 function M.config()
   local null_ls = require "null-ls"
-  -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+  -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins
   local formatting = null_ls.builtins.formatting
-  -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
   local diagnostics = null_ls.builtins.diagnostics
+  local code_actions = null_ls.builtins.code_actions
+  local hover = null_ls.builtins.hover
+  local completion = null_ls.builtins.completion
 
-  -- https://github.com/prettier-solidity/prettier-plugin-solidity
   null_ls.setup {
     debug = false,
     sources = {
-      formatting.prettier.with {
-        extra_filetypes = { "toml" },
-        extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-      },
-      formatting.black.with { extra_args = { "--fast" } },
+      -- python TBD
+      -- formatting.black.with { extra_args = { "--fast" } },
+      -- formatting.blue,
+      -- formatting.isort,
+      -- formatting.usort,
+      -- formatting.pyink,
+      -- formatting.pyflyby,
+      -- formatting.yapf,
+      -- formatting.ruff,
+      -- formatting.autoflake,
+      -- formatting.autopep8,
+      -- diagnostics.ruff,
+      -- formatting.reorder_python_imports,
+      -- diagnostics.flake8,
+      -- diagnostics.mypy,
+      -- diagnostics.pycodestyle,
+      -- diagnostics.pydocstyle,
+      -- diagnostics.pylama,
+      -- diagnostics.pylint,
+      -- diagnostics.vulture,
+
+      -- lua
       formatting.stylua,
-      formatting.google_java_format,
-      diagnostics.flake8,
+      -- formatting.lua_format,
+      diagnostics.luacheck,
+      -- diagnostics.selene,
+
+      -- golang
+      formatting.gofmt,
+      -- formatting.goimports,
+      -- formatting.gofumpt,
+      -- formatting.golines,
+      -- code_actions.gomodifytags,
+      -- code_actions.impl,
+      -- diagnostics.golangci_lint,
+      -- diagnostics.gospel,
+      -- formatting.goimports_reviser,
+      -- diagnostics.revive,
+      -- diagnostics.staticcheck,
+
+      -- shell TBD
+      -- formatting.shfmt,
+      -- formatting.beautysh,
+      -- formatting.shellharden,
+      -- code_actions.shellcheck,
+      -- diagnostics.shellcheck,
+      -- diagnostics.zsh,
+
+      -- crystal
+      -- formatting.crystal_format,
+
+      -- terraform
+      -- formatting.terraform_fmt,
+
+      -- json
+      diagnostics.jsonlint,
+      -- formatting.json_tool,
+
+      -- ruby TBD
+      -- formatting.rubocop,
+      -- diagnostics.rubocop,
+      -- formatting.standardrb,
+      -- formatting.htmlbeautifier,
+      -- formatting.rubyfmt,
+      -- formatting.rufo,
+      -- diagnostics.erb_lint,
+      -- diagnostics.reek,
+
+      -- ansible
+      diagnostics.ansiblelint.with { temp_dir = "/tmp" },
+
+      -- markdown TBD
+      -- formatting.markdownlint,
+      diagnostics.markdownlint,
+      -- formatting.markdown_toc,
+      formatting.mdformat.with { extra_args = { "--wrap=80", "--number" } },
+      -- formatting.ocdc,
+      -- formatting.remark,
+      -- formatting.terrafmt,
+      -- diagnostics.mdl,
+      -- formatting.cbfmt,
+
+      -- yaml
+      diagnostics.yamllint,
+      formatting.yamlfmt.with { extra_args = { "-formatter", "include_document_start=true" } },
+      -- formatting.yq,
+
+      -- docker
+      diagnostics.hadolint,
+
+      -- general
+      formatting.prettier.with { filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "css", "scss", "less", "html", "graphql", "handlebars", "toml" }, extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } },
+      formatting.trim_whitespace,
+      formatting.trim_newlines,
+      diagnostics.trail_space,
+      diagnostics.todo_comments,
+      hover.printenv,
+      -- diagnostics.semgrep,
+      -- code_actions.refactoring,
+      -- formatting.deno_fmt,
+      -- formatting.dprint,
+
+      -- english TBD
+      hover.dictionary,
+      diagnostics.misspell
+      -- completion.spell.with { filetypes = { "markdown" } },
+      -- diagnostics.write_good,
+      -- code_actions.proselint,
+      -- diagnostics.alex,
+      -- diagnostics.textidote,
+      -- formatting.textlint,
+      -- diagnostics.vale,
+      -- diagnostics.typos,
+      -- diagnostics.cspell,
+      -- diagnostics.codespell.with({ filetypes = { "markdown", "go" } }),
+      -- formatting.codespell.with({ filetypes = { "markdown", "go" } }),
+      -- diagnostics.commitlint,
     },
+  }
+
+  require("mason-null-ls").setup {
+    ensure_installed = nil,
+    automatic_installation = true,
   }
 end
 
--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -1,14 +1,65 @@
 local M = {}
 
 M.servers = {
-  "lua_ls",
-  "cssls",
-  "html",
-  "tsserver",
-  "pyright",
-  "bashls",
+  "ansiblels",
+  -- "awk_ls",
+  -- "bashls",
+  -- "cssls",
+  -- "graphql",
+  -- "html",
+  "helm_ls",
   "jsonls",
+  "lua_ls",
+  -- "sqlls",
+  -- "sqls",
+  -- "vimls",
   "yamlls",
+  -- "zk"
+  -- "ltex",
+
+  -- markdown
+  "marksman",
+
+  -- crytsal
+  -- "crystalline",
+  -- "scry",
+
+  -- docker
+  "dockerls",
+  "docker_compose_language_service",
+
+  -- golang
+  "golangci_lint_ls",
+  "gopls",
+
+  -- english
+  -- "prosemd_lsp",
+  -- "grammarly",
+  -- "remark_ls",
+  -- "textlsp",
+  -- "vale_ls",
+
+  -- python
+  -- "pylsp",
+  -- "pylyzer",
+  -- "pyre",
+  -- "pyright",
+  -- "ruff_lsp",
+  -- "sourcery",
+
+  -- ruby
+  -- "standardrb",
+  -- "rubocop",
+  -- "ruby_ls",
+  -- "solargraph",
+  -- "sorbet",
+  -- "steep",
+  -- "syntax_tree",
+  -- "typeprof",
+
+  -- terraform
+  -- "terraformls",
+  -- "tflint",
 }
 
 return M
