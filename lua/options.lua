1e6fcc6c69a67e03713261e0f1fab1e613fc469d
Jon Doveston <jon@doveston.me.uk>
2023-08-26 23:01:23 +0100

    Add some options

--- lua/options.lua
+++ lua/options.lua
@@ -41,3 +41,9 @@ vim.opt.whichwrap:append "<,>,[,],h,l"          -- keys allowed to move to the p
 vim.opt.iskeyword:append "-"                    -- treats words with `-` as single words
 vim.opt.formatoptions:remove { "c", "r", "o" }  -- This is a sequence of letters which describes how automatic formatting is to be done
 vim.opt.linebreak = true
+
+vim.opt.textwidth = 80
+vim.opt.colorcolumn = '80'
+vim.opt.wildignore = { '*/cache/*', '*/tmp/*', '*.so', '*.swp', '*.zip' }
+vim.opt.listchars = { eol = '↲', tab = '▸ ', trail = '·' }
+vim.opt.modeline = false
