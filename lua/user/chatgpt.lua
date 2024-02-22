c3bc5b3141d326b8a1cdfc12a905bd4817362abc
Jon Doveston <jon@doveston.me.uk>
2024-02-22 08:34:55 +0000

    Add chatgpt

---
+++ lua/user/chatgpt.lua
@@ -0,0 +1,16 @@
+local M = {
+  "jackMort/ChatGPT.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "folke/trouble.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+}
+
+function M.config()
+    require("chatgpt").setup()
+end
+
+return M
