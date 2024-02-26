fd671840069590b60ab8af191c702028ae81b60c
Jon Doveston <jon@doveston.me.uk>
2024-02-26 16:17:38 +0000

    Add gitlab plugin

--- lua/user/gitlab.lua
+++ lua/user/gitlab.lua
@@ -1,17 +1,21 @@
--- local M = {
---   url = "git@gitlab.com:gitlab-org/editor-extensions/gitlab.vim.git",
---   lazy = false,
--- }
---
--- M.name = "gitlab"
--- function M.config()
---   require('gitlab').setup {
---   statusline = {
---     enabled = false
---   }
--- }
---
--- end
---
--- return M
-return {}
+local M = {
+  -- 'git@gitlab.com:gitlab-org/editor-extensions/gitlab.vim.git',
+  'https://gitlab.com/gitlab-org/editor-extensions/gitlab.vim.git',
+  event = { 'BufReadPre', 'BufNewFile' },
+  ft = { 'go', 'javascript', 'python', 'ruby' },
+  cond = function()
+    return vim.env.GITLAB_TOKEN ~= nil and vim.env.GITLAB_TOKEN ~= ''
+  end,
+  opts = {
+    statusline = {
+      enabled = false,
+    },
+  },
+}
+
+M.name = "gitlab"
+function M.config()
+  require("gitlab").setup()
+end
+
+return M
