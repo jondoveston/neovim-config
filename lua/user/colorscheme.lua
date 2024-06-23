190cf8a79112078936516bcdc76fb08d788de516
Jon Doveston <jon@doveston.me.uk>
2024-06-23 18:20:08 +0100

    Switch colorscheme to tokyonight

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,11 +1,23 @@
+-- https://github.com/folke/tokyonight.nvim
 local M = {
-  "LunarVim/darkplus.nvim",
+  "folke/tokyonight.nvim",
+  -- "LunarVim/darkplus.nvim",
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
 }
 
+-- M.name = "tokyonight-night"
 function M.config()
-  vim.cmd.colorscheme "darkplus"
+  vim.cmd.colorscheme "tokyonight-night"
+
+  -- local status_ok, _ = pcall(vim.cmd.colorscheme, M.name)
+  -- if not status_ok then
+  --   return
+  -- end
+
+  -- require("tokyonight").setup {
+  --   style = "storm"
+  -- }
 end
 
 return M
