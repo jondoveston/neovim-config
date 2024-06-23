45ffed2bbe681eb0b1e55488d464d6df9f1d2318
Jon Doveston <jon@doveston.me.uk>
2024-06-23 21:40:21 +0100

    Tweak lualine configuration

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -16,7 +16,7 @@ function M.config()
     sections = {
       lualine_a = {},
       lualine_b = { "branch" },
-      lualine_c = { "diagnostics" },
+      lualine_c = { "diagnostics", { 'filename', path = 1, } },
       lualine_x = { "copilot", "filetype" },
       lualine_y = { "progress" },
       lualine_z = {},
