c088cda71dce96df6f03ca8b2e872a6976ae3730
Jon Doveston <jon@doveston.me.uk>
2024-06-23 20:45:39 +0100

    Configure mason for lsp servers and tools

--- lua/user/mason.lua
+++ lua/user/mason.lua
@@ -4,30 +4,40 @@
 local M = {
   "williamboman/mason-lspconfig.nvim",
   dependencies = {
-    "williamboman/mason.nvim",
+    {
+      "williamboman/mason.nvim",
+    },
+    {
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
   },
 }
 
-
 function M.config()
-  local servers = {
-    "lua_ls",
-    "cssls",
-    "html",
-    "tsserver",
-    "pyright",
-    "bashls",
-    "jsonls",
+  -- ensure lsp servers in lspconfig server name format
+  -- for canonical list see lspconfig
+  local servers = {}
+
+  -- ensure other tools in mason name format
+  local utils = {
+    "shellcheck",
+    -- "gotests",
+    -- "misspell",
   }
 
   require("mason").setup {
     ui = {
       border = "rounded",
     },
+    PATH = "append", -- this will allow the system tools to override Mason
   }
 
   require("mason-lspconfig").setup {
     ensure_installed = servers,
+    automatic_installation = true,
+  }
+  require("mason-tool-installer").setup {
+    ensure_installed = utils,
   }
 end
 
